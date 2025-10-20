05eaec30b64205571eaf61744d63fdeace39014f
yara123456789yara <160149907+yara123456789yara@users.noreply.github.com>
2025-10-20 16:55:25 +0300

    middleware

--- server.js
+++ server.js
@@ -1,5 +1,18 @@
 const express = require('express')
+const dbConnection = require('./database/dbConnections.js')
+const globalError = require('./src/middleware/errorHander.js')
 const app = express()
 const port = 3000
+app.use(express.json())
+dbConnection()
 
+
+
+
+
+
+
+
+
+app.use(globalError)
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
